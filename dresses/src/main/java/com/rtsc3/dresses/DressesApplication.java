1461d552dc76923b9a922a448b53d32522ce3104
Leonardo Rosero <danielre200@gmail.com>
2022-10-17 12:26:59 -0500

    R3Ciclo3

---
+++ dresses/src/main/java/com/rtsc3/dresses/DressesApplication.java
@@ -0,0 +1,13 @@
+package com.rtsc3.dresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressesApplication.class, args);
+	}
+
+}
